b521dd0cfd09ced6e49c593f9860355873fc9e42
Luzefiru <ian.deejay.iv@gmail.com>
2023-01-28 19:35:10 +0800

    feat: add playRound() function

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+/**
+ * WARNING: Does not error handle input outside the allowable strings listed below.
+ * 
+ * Acceptable string inputs are listed in this array: ['Rock', 'Paper', 'Scissors'].
+ * It is not case-sensitive.
+ * 
+ * @param {string} playerChoice
+ * @param {string} computerChoice 
+ * @returns a string which is the message to display at the result of a round of Rock Paper Scissors
+ */
+function playRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (playerChoice == computerChoice)
+        return "It's a tie!";
+    
+    switch (playerChoice)
+    {
+        case 'rock':
+            return (computerChoice == 'scissors')
+            ? "You win! Rock beats Scissors."
+            : "You lose. Paper beats Rock.";
+
+        case 'paper':
+            return (computerChoice == 'rock')
+            ? "You win! Paper beats Rock."
+            : "You lose. Scissors beats Paper.";
+
+        case 'scissors':
+            return (computerChoice == 'paper')
+            ? "You win! Scissors beats Paper."
+            : "You lose. Rock beats Scissors.";
+    }
+}
 
 /**
  * @returns a string which is a random selection from; 'Rock', 'Paper', 'Scissors'
